13b57c0ade23536cbb11cbfd9c7deb9207d199aa
Abhi <abhishekchauhan9036@gmail.com>
2022-06-01 21:26:08 +0530

    commit message

---
+++ Chef-and-Chocolates.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+     ll x,y,z;
+     cin>>x>>y>>z;
+     cout<<(5*x + 10*y)/z<<endl;
+    }
+    return 0;
+}

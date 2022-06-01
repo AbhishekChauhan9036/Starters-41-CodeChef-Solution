1c788f993c8899246567129786dbaeb0eda44698
Abhi <abhishekchauhan9036@gmail.com>
2022-06-01 20:10:44 +0530

    commit message

--- chef-and-Brain-Speed.cpp
+++ chef-and-Brain-Speed.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 
 int main() {
-	// your code goes here
+	
 	int a,b;
 	
 	cin>>a>>b;
 	
 	if(a<b){
-	    cout<<"YES"<<endl;
+	    cout<<"YES";
 	}else{
-	    cout<<"NO"<<endl;
+	    cout<<"NO";
 	}
 	return 0;
 }
